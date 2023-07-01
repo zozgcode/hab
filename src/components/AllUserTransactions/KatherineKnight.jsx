a5a6ad226939d44638a6e14c190f367ab56f95f4
sunshine-web-coder <webdev4gaming@gmail.com>
2023-07-01 16:33:43 +0100

    first commit

---
+++ src/components/AllUserTransactions/KatherineKnight.jsx
@@ -0,0 +1,181 @@
+import React from "react";
+
+export default function KatherineKnight() {
+  return (
+    <div className="transaction-history">
+      <div className="transaction-history-header">
+        <p>LAST TRANSACTIONS</p>
+        <div className="transaction-history-header-right">
+          <p>
+            <svg
+              width="12"
+              height="9"
+              viewBox="0 0 18 9"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.63605 0.244781L17.3841 8.62065L8.22768 8.7214L0.379285 8.80775L9.63605 0.244781Z"
+                fill="#1CBD19"
+              />
+            </svg>
+            &nbsp; $8,000.00
+          </p>
+          <p>
+            <svg
+              width="12"
+              height="9"
+              viewBox="0 0 17 9"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7.74643 8.56L-1.8195e-05 0.185913L9.15384 0.0857985L17 -1.39723e-05L7.74643 8.56Z"
+                fill="#A11C1C"
+              />
+            </svg>
+            &nbsp; $-261.81
+          </p>
+        </div>
+      </div>
+      <div className="transaction">
+        <div className="transaction_box transaction_boxxx">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/247229.mhy/cre <p>23/6/2016</p>
+            </span>
+            <span className="green">$245,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/063741.mhy/cre <p>15/6/2015</p>
+            </span>
+            <span className="green">$2,800,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box transaction_boxxx">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/344086.mhy/cre <p>23/5/2014</p>
+            </span>
+            <span className="green">$315,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/212536.mhy/cre <p>1/5/2013</p>
+            </span>
+            <span className="green">$200,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box transaction_boxxx">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/22456.mhy/cre<p>25/4/2012</p>
+            </span>
+            <span className="green">$473,750.00</span>
+          </div>
+        </div>
+        <div className="transaction_box">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/85256.mhy/cre <p>1/3/2011</p>
+            </span>
+            <span className="green">+$825,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box">
+          <div className="transaction_box-top transaction_boxxx">
+            <span className="_desc">
+              tx/07536.mhy/cre <p>10/2/2010</p>
+            </span>
+            <span className="green">+$1,500,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/262536.mhy/cre <p>4/1/2009</p>
+            </span>
+            <span className="green">+$750,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box transaction_boxxx">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/22446.mhy/cre <p>10/12/2008</p>
+            </span>
+            <span className="green">+$50,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/13456.mhy/cre <p>10/05/2007</p>
+            </span>
+            <span className="green">+$180,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box transaction_boxxx">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/20156.mhy/cre <p>20/07/2006</p>
+            </span>
+            <span className="green">+$250,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/52420.mhy/cre <p>13/01/2005</p>
+            </span>
+            <span className="green">+$150,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box transaction_boxxx">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/13739.mhy/cre <p>20/07/2004</p>
+            </span>
+            <span className="green">+$250,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/33689.mhy/cre <p>13/01/2003</p>
+            </span>
+            <span className="green">+$150,000.00</span>
+          </div>
+        </div>
+        <div className="transaction_box transaction_boxxx">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/25780.mhy/cre <p>20/07/2002</p>
+            </span>
+            <span className="green">+$473,750.00</span>
+          </div>
+        </div>
+        <div className="transaction_box">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/32489.mhy/cre <p>13/01/2001</p>
+            </span>
+            <span className="green">+$173,750.00</span>
+          </div>
+        </div>
+        <div className="transaction_box transaction_boxxx">
+          <div className="transaction_box-top">
+            <span className="_desc">
+              tx/11244.mhy/cre <p>20/07/2000</p>
+            </span>
+            <span className="green">+$473,750.00</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/userDB.jsx
@@ -0,0 +1,14 @@
+const users = [
+  { 
+    id: 1,
+    name: "Katherine Knight",
+    cardName: "Katherine Knight",
+    username: "Myself&Myman",
+    password: "Kathe1313",
+    amount: "7,580,000.00",
+  },
+];
+export default users;
+
+
+
