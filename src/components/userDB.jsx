618bdcc1f9349ebf69d26b5267908b81aadb1ad2
sunshine-web-coder <webdev4gaming@gmail.com>
2023-07-02 03:51:28 +0100

    l

--- src/components/userDB.jsx
+++ src/components/userDB.jsx
@@ -3,8 +3,8 @@ const users = [
     id: 1,
     name: "Katherine Knight",
     cardName: "Katherine Knight",
-    username: "Kathe13139999",
-    password: "Myself&Myman999",
+    username: "Kathe1313",
+    password: "Myself&Myman",
     amount: "7,580,000.00",
   },
   { 
