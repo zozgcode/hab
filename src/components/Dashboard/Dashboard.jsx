f1eefc572111128e62c08bcd77da2829cd19c181
sunshine-web-coder <webdev4gaming@gmail.com>
2023-10-19 14:44:08 +0100

    Recipient text changed

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -87,7 +87,7 @@ export default function Dashboard() {
       >
         <form className="modal_form" onSubmit={(e) => e.preventDefault()}>
           <div>
-            <label>Receipt Account</label>
+            <label>Recipient Account</label>
             <input type="text" />
           </div>
           <div>
